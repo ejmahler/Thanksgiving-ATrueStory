a59afcee13f964b2aac5f4306c2e9c50943f9e67
Elliott Mahler <join.together@gmail.com>
2016-01-29 22:10:31 -0800

    Added stuff

---
+++ Assets/__Scripts/HelloBrian.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloBrian : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
