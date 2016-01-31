0110dec178796d866a521c09a3efd15494dbe983
Elliott Mahler <join.together@gmail.com>
2016-01-30 20:40:40 -0800

    finished sacrifice scene

--- Assets/__Scripts/TurkeySacrificeStory.cs
+++ Assets/__Scripts/TurkeySacrificeStory.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class TurkeySacrificeStory : MonoBehaviour {
 
-	private string initialStory = "something about a burned name";
+	private string[] initialStory = {
+		"Every morning on November 1st, the townspeople awake...",
+		"...To find a name scorched into the ground.",
+
+	};
 	private string[] restOfStory = {
-		"kid has to sacrifice",
-		"[i]...or else[/i]",
-		"on the day after thanksgiving, if the witches were satisfied, the scorched name disappeared without a trace"
+		"Whoever is named by the curse must sacrifice their pet turkey to the witches on Thanksgiving Day...",
+		"<i>...Or Else.</i>",
+		"The next day, if the witches are satisfied, the scorched name disappears without a trace"
 	};
 
 	[SerializeField]
@@ -25,21 +30,47 @@ public class TurkeySacrificeStory : MonoBehaviour {
 	private Text instructionText;
 
 	[SerializeField]
+	private GameObject blackBackground;
+	[SerializeField]
 	private GameObject initialBackground;
+
+	private int numStabs = 0;
 
 	private float currentKnifePosition = 0f;
 
 	// Use this for initialization
 	IEnumerator Start () {
-		storyText.text = initialStory;
+		storyText.text = initialStory[0];
+		instructionText.text = "Press [Space] to continue";
 
-		yield return StartCoroutine (WaitForStabs (2));
+		while (!Input.GetKeyDown (KeyCode.Space)) {
+			yield return null;
+		}
+		blackBackground.SetActive (false);
+		storyText.text = initialStory[1];
+
+		yield return new WaitForSeconds (.5f);
+
+		while (!Input.GetKeyDown (KeyCode.Space)) {
+			yield return null;
+		}
+		initialBackground.SetActive (false);
+
+		instructionText.text = "Move the mouse left and right to sacrifice the turkey";
+		StartCoroutine (WatchForStabs ());
 
 		foreach (string storyItem in restOfStory) {
 			storyText.text = storyItem;
 
-			yield return StartCoroutine (WaitForStabs (2));
+			yield return new WaitForSeconds (3);
+
+			numStabs = 0;
+			yield return StartCoroutine (WaitForStabs (3));
 		}
+
+		//story is over
+		var result = SceneManager.LoadSceneAsync("ThrowCandy", LoadSceneMode.Single);
+		result.allowSceneActivation = true;
 	}
 	
 	// Update is called once per frame
@@ -51,14 +82,25 @@ public class TurkeySacrificeStory : MonoBehaviour {
 		currentKnifePosition = Mathf.InverseLerp (minKnifeX, maxKnifeX, knifePos.x);
 	}
 
-	private IEnumerator WaitForStabs(int numStabs) {
-		for (int i = 0; i < numStabs; i++) {
+	private IEnumerator WatchForStabs() {
+		while(true) {
 			while (currentKnifePosition < .75f) {
 				yield return null;
 			}
+			GetComponent<AudioSource> ().Play ();
 			while (currentKnifePosition > .25f) {
 				yield return null;
 			}
+			numStabs++;
+		}
+	}
+
+	private IEnumerator WaitForStabs(int count) {
+		for (int i = 0; i < count; i++) {
+			while (numStabs == 0) {
+				yield return null;
+			}
+			numStabs--;
 		}
 	}
 }
