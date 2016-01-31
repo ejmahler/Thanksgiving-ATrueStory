108ade10227e46b708777d9c2d2eedbc3f81763e
Elliott Mahler <join.together@gmail.com>
2016-01-31 14:36:49 -0800

    forgot files

---
+++ Assets/__Scripts/TextFlasher.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextFlasher : MonoBehaviour {
+
+	private RectTransform blackBackground;
+	private Text textBox;
+
+	// Use this for initialization
+	void Start () {
+		blackBackground = GetComponentInParent<Image> ().GetComponent<RectTransform> ();
+		textBox = GetComponent<Text> ();
+	}
+	
+	public void SetText(string text) {
+		textBox.text = text;
+
+		LeanTween.color (blackBackground, Color.gray, .2f).setLoopPingPong (1);
+	}
+}
