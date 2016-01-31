6f577a3d7dbf16415a60e108496d39477945e9ad
Brian Pickens <brianpickens53@gmail.com>
2016-01-30 20:04:38 -0800

    safety commit

---
+++ Assets/__Scripts/Menu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void England(){
+		Application.LoadLevel ("AMURICA");
+	}
+
+	public void Reaper(){
+		Application.LoadLevel ("ReapingOfTurkeys");
+	}
+
+	public void Witches(){
+		Application.LoadLevel ("SacrificingTurkey");
+	}
+
+	public void Famine(){
+		Application.LoadLevel ("");
+	}
+
+	public void Prophecy(){
+		Application.LoadLevel ("");
+	}
+
+	public void Credits(){
+
+	}
+}
